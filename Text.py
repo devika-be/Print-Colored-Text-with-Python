1fccd45341c011258c39ab9c4253acedaa2bc398
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2022-08-26 00:43:30 -0400

    Add files via upload

---
+++ Text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Devika Pagare "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Devika Pagare")
+print(Fore.RED + Back.GREEN+ "Hi My name is Devika Pagare")
